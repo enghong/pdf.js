97474f26f9af4a25dc8a75ed56b13240a78722d3
Eng-Hong Sron <enghong.sron@gmail.com>
2024-10-10 15:52:47 +0200

    Allow Kimayo origins

--- web/app.js
+++ web/app.js
@@ -2211,9 +2211,17 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
   const HOSTED_VIEWER_ORIGINS = [
     "null",
-    "http://mozilla.github.io",
-    "https://mozilla.github.io",
+    "http://localhost:3000",
+    "https://app.kimayo.io",
+    "https://app.kimayo.com",
   ];
+
+  const HOSTED_FILE_ORIGINS = [
+    "https://kimayo-documents.s3.eu-west-3.amazonaws.com",
+    "https://kimayo-documents-test.s3.eu-west-3.amazonaws.com",
+    "https://kimayo-documents-staging.s3.eu-west-3.amazonaws.com",
+  ];
+
   // eslint-disable-next-line no-var
   var validateFileURL = function (file) {
     if (!file) {
@@ -2226,12 +2234,18 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
         return;
       }
       const fileOrigin = new URL(file, window.location.href).origin;
+      if (fileOrigin === viewerOrigin) {
+        return;
+      }
+
+      if (HOSTED_FILE_ORIGINS.includes(fileOrigin)) {
+        return;
+      }
+
       // Removing of the following line will not guarantee that the viewer will
       // start accepting URLs from foreign origin -- CORS headers on the remote
       // server must be properly configured.
-      if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
-      }
+      throw new Error("file origin does not match viewer's");
     } catch (ex) {
       PDFViewerApplication._documentError("pdfjs-loading-error", {
         message: ex.message,
